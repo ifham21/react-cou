ac3bf63a94be0042b96739e286a90e30788bd0ca
Ahmed Ifham <ifham.maa@gmail.com>
2025-02-18 23:43:54 +0530

    Updates

--- src/Course/CourseList.jsx
+++ src/Course/CourseList.jsx
@@ -24,6 +24,12 @@ const CourseList = () => {
             image: jsimg, 
             rating: 5,
         },
+        {
+            course: 'Javascript',
+            price: '$50', 
+            image: jsimg, 
+            rating: 5,
+        },
 
     ]
 
