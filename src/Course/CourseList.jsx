85b933a3453dcf57f449693a854a2b96a6f065ce
Ahmed Ifham <ifham.maa@gmail.com>
2025-02-20 23:49:39 +0530

    Made the code clean and added the courseList inside the fragment

--- src/Course/CourseList.jsx
+++ src/Course/CourseList.jsx
@@ -1,41 +1,47 @@
-import React from 'react'
-import htmlimg from './assets/html.jpg'
-import cssimg from './assets/css.jpg'
-import jsimg from './assets/js.png'
+import React from "react";
+import htmlimg from "./assets/html.jpg";
+import cssimg from "./assets/css.jpg";
+import jsimg from "./assets/js.png";
 
 const CourseList = () => {
+  const courses = [
+    {
+      course: "HTML",
+      price: "$20",
+      image: htmlimg,
+      rating: 5,
+    },
+    {
+      course: "CSS",
+      price: "$20",
+      image: cssimg,
+      rating: 5,
+    },
+    {
+      course: "Javascript",
+      price: "$30",
+      image: jsimg,
+      rating: 5,
+    },
+  ];
 
-    const courses = [
-        {
-            course: 'HTML',
-            price: '$20', 
-            image: htmlimg, 
-            rating: 5,
-        },
-        {
-            course: 'CSS',
-            price: '$20', 
-            image: cssimg, 
-            rating: 5,
-        },
-        {
-            course: 'Javascript',
-            price: '$30', 
-            image: jsimg, 
-            rating: 5,
-        },
-
-    ]
-
-    const courseList = courses.map((course, index) => {
-        return(
-            <Course name={course.name} image ={course.image} price={course.price} rating={course.rating} key={index}/>
-        )
-    })
+  const courseList = courses.map((course, index) => {
+    return (
+      <Course
+        name={course.name}
+        image={course.image}
+        price={course.price}
+        rating={course.rating}
+        key={index}
+      />
+    );
+  });
 
   return (
-    <div>CourseList</div>
+    <>
+        {courseList}
+    </>
   )
-}
+};
 
-export default CourseList
+export default CourseList;
